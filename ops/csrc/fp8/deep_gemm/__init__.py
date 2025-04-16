5b888800a6cab16742b1731b78c41b652c157463
chen2016013 <cx2016013@163.com>
2025-04-16 20:15:25 +0800

    update DeepGEMM

--- ops/csrc/fp8/deep_gemm/__init__.py
+++ ops/csrc/fp8/deep_gemm/__init__.py
@@ -18,13 +18,12 @@
 
 from . import jit
 from .jit_kernels import (
-    ceil_div,
     gemm_fp8_fp8_bf16_nt,
-    get_col_major_tma_aligned_tensor,
-    get_m_alignment_for_contiguous_layout,
-    get_num_sms,
     m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
     m_grouped_gemm_fp8_fp8_bf16_nt_masked,
-    set_num_sms,
+    ceil_div,
+    set_num_sms, get_num_sms,
+    get_col_major_tma_aligned_tensor,
+    get_m_alignment_for_contiguous_layout
 )
 from .utils import bench, calc_diff, get_cuda_home
